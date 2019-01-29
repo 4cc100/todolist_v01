4599904a05cdf97b5d5f750f0c83c9f93ad525ac
4cc100 <vikcsi.varga@gmail.com>
2019-01-29 21:07:38 +0100

    implement delete from db

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
  
   Item.find({}, (err, results) => {
     if(results.length === 0) {
@@ -54,26 +54,38 @@ app.get('/', function(req, res){
 
 });
 
-app.post('/', function(req, res) {
-  const item = req.body.newItem;
+app.post('/', (req, res) => {
+  const itemName = req.body.newItem;
 
-  if(req.body.list === "Work"){
-    workItems.push(item);
-    res.redirect('/work');
-  } else {
-    items.push(item);
-    res.redirect('/');
-  }
+  const newItem = new Item({
+    name: itemName
+  });
+
+  newItem.save();
+  res.redirect('/');
+
 });
 
-app.get('/work', function(req, res) {
+app.post('/delete', (req, res) => {
+  const checkedItemId = req.body.checkbox;
+  Item.findByIdAndRemove({_id: checkedItemId}, (err) => {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("Deleted item successfully");
+      res.redirect('/');
+    }
+  })
+});
+
+app.get('/work', (req, res) => {
   res.render('list', {listTitle: "Work List", newListItems: workItems});
 });
 
-app.get('/about', function(req, res) {
+app.get('/about', (req, res) => {
   res.render('about');
 });
 
-app.listen(3000, function() {
+app.listen(3000, () => {
   console.log("The server is running on port 3000");
 });
